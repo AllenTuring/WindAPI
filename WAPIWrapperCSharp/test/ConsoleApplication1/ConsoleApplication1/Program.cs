8027b4f0d19cb5b441022e70d70ee819d59f278e
Allen Jiang <allen.jiang@outlook.com>
2015-07-16 15:20:16 +0800

    Committed untracked related files and added two blank "test" folder template projects for console and Windows Forms applications taking advantage of the Wind API

---
+++ WAPIWrapperCSharp/test/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WAPIWrapperCSharp;
+using WindCommon;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WindAPI w = new WindAPI();
+            w.start();
+
+            WindData wd = w.wsd("600000.SH", "MACD", "ED-1M", "2014-12-16", "MACD_L=26;MACD_S=12;MACD_N=9;MACD_IO=1;Fill=Previous");
+            
+            
+            string str = WindDataMethod.WindDataToString(wd, "wsd");
+            Console.Write(str);
+            Console.Read();
+
+            w.stop();
+        }
+    }
+}
---
+++ WAPIWrapperCSharp/test/HelloWindAPI/HelloWindAPI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WAPIWrapperCSharp;
+
+namespace HelloWindAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WindAPI w = new WindAPI();
+            w.start();
+
+
+
+            w.stop();
+        }
+    }
+}
